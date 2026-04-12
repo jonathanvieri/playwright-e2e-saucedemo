7c27c69445dc081b73e0d6cbbac2c90c6ca95a5d
jonathanvieri <jonathanvieri.dev@gmail.com>
2026-04-12 20:56:18 +0700

    Add data-driven tests
    - Add testData.ts containing checkoutUsers
    - Update checkout spec to use testData.ts

--- tests/checkouts.spec.ts
+++ tests/checkouts.spec.ts
@@ -2,27 +2,30 @@ import { test, expect } from './fixtures';
 import { InventoryPage } from '../pages/InventoryPage';
 import { CartPage } from '../pages/CartPage';
 import { CheckoutPage } from '../pages/CheckoutPage';
+import { checkoutUsers } from '../utils/testData';
 
-test('user can complete checkout flow', async ({ loggedInPage }) => {
-  const inventoryPage = new InventoryPage(loggedInPage);
-  const cartPage = new CartPage(loggedInPage);
-  const checkoutPage = new CheckoutPage(loggedInPage);
+checkoutUsers.forEach((user) => {
+  test(`user can complete checkout flow - ${user.firstName}`, async ({ loggedInPage }) => {
+    const inventoryPage = new InventoryPage(loggedInPage);
+    const cartPage = new CartPage(loggedInPage);
+    const checkoutPage = new CheckoutPage(loggedInPage);
 
-  // Add item
-  inventoryPage.addBackpackToCart();
+    // Add item
+    inventoryPage.addBackpackToCart();
 
-  // Go to cart
-  cartPage.goToCart();
+    // Go to cart
+    cartPage.goToCart();
 
-  // Checkout
-  await cartPage.clickCheckout();
+    // Checkout
+    await cartPage.clickCheckout();
 
-  await checkoutPage.fillInformation('Shiba', 'Inu', '12345');
-  await checkoutPage.continueCheckout();
+    await checkoutPage.fillInformation(user.firstName, user.lastName, user.postalCode);
+    await checkoutPage.continueCheckout();
 
-  await checkoutPage.finishCheckout();
+    await checkoutPage.finishCheckout();
 
-  // Verify success
-  await expect(checkoutPage.getSuccessHeader()).toBeVisible();
-  await expect(checkoutPage.getSuccessMessage()).toBeVisible();
+    // Verify success
+    await expect(checkoutPage.getSuccessHeader()).toBeVisible();
+    await expect(checkoutPage.getSuccessMessage()).toBeVisible();
+  });
 });
---
+++ utils/testData.ts
@@ -0,0 +1,12 @@
+export const checkoutUsers = [
+  {
+    firstName: 'LeBron',
+    lastName: 'James',
+    postalCode: '90001',
+  },
+  {
+    firstName: 'Mary',
+    lastName: 'Jane',
+    postalCode: '07008',
+  },
+];
